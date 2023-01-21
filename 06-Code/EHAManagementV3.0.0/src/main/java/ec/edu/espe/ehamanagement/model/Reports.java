073495e9207647bc8f275b196f83ac6c8f63b5fe
Jairo Bonilla <jsbonilla2@espe.edu.ec>
2023-01-20 21:12:58 -0500

    reporting implementation

---
+++ 06-Code/EHAManagementV3.0.0/src/main/java/ec/edu/espe/ehamanagement/model/Reports.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.ehamanagement.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Jairo Bonilla, Gaman GeekLords, DCCO-ESPE
+ */
+public  abstract class Reports 
+{
+     private int id;
+     private String type;
+     private Date date;
+}
