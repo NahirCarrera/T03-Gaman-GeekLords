4872896b28749b1c02f44366075b7cc6e7107e82
Jairo Bonilla <jsbonilla2@espe.edu.ec>
2023-02-14 16:06:02 -0500

    unit test implementation

--- 06-Code/EHAManagementV3.2.0/src/main/java/utils/CostsCalculator.java
+++ 06-Code/EHAManagementV3.2.0/src/main/java/utils/CostsCalculator.java
@@ -16,49 +16,57 @@ import java.util.ArrayList;
  * @author Nahir Carrera, Gaman GeekLords, DCC0-ESPE
  */
 public class CostsCalculator {
- 
-    public  static float computeMaterialUnitCost(Material material) throws ParseException{
+
+    public static float computeMaterialUnitCost(Material material) throws ParseException {
         float totalCost = material.getGeneralCost();
         double quantity = material.getGeneralQuantity();
-        float unitQuantity = (float) (totalCost/quantity);
-        unitQuantity =  roundToTwoDecimalPlaces(unitQuantity);
-        System.out.println("material unit quantity ->" +unitQuantity);
+        float unitQuantity = (float) (totalCost / quantity);
+        unitQuantity = roundToTwoDecimalPlaces(unitQuantity);
+        System.out.println("material unit quantity ->" + unitQuantity);
         return unitQuantity;
     }
-    
-    private  static float computeMaterialsCostPerProduct( MongoCollection materialsCollection, Product product){
-        ArrayList materialsListPerProduct  = DictionaryConversor.convertToArrayList(product.getMaterials(), "keys");
+
+    private static float computeMaterialsCostPerProduct(MongoCollection materialsCollection, Product product) {
+        ArrayList materialsListPerProduct = DictionaryConversor.convertToArrayList(product.getMaterials(), "keys");
         ArrayList materialsQuantitiesPerProduct = DictionaryConversor.convertToArrayList(product.getMaterials(), "values");
         float totalMaterialsCostPerProduct = 0;
-        if(!materialsListPerProduct.isEmpty()){
-            for(int i = 0; i< materialsListPerProduct.size(); i++){ 
+        if (!materialsListPerProduct.isEmpty()) {
+            for (int i = 0; i < materialsListPerProduct.size(); i++) {
                 double materialId = Double.valueOf(String.valueOf(materialsListPerProduct.get(i)));
-                float materialUnitCost = Float.parseFloat(String.valueOf(MaterialsOrganizer.getValueFromMaterial(materialsCollection, "id",(int)materialId, "unitCost")));
-                float materialQuantity =  Float.valueOf(String.valueOf(materialsQuantitiesPerProduct.get(i)));
+                float materialUnitCost = Float.parseFloat(String.valueOf(MaterialsOrganizer.getValueFromMaterial(materialsCollection, "id", (int) materialId, "unitCost")));
+                float materialQuantity = Float.valueOf(String.valueOf(materialsQuantitiesPerProduct.get(i)));
                 float materialTotalCost = materialUnitCost * materialQuantity;
                 totalMaterialsCostPerProduct += materialTotalCost;
-                }
+            }
         }
         return totalMaterialsCostPerProduct;
     }
-    
-    private static float computeWorkingTimeCostPerProduct(MongoCollection userCollection, Product product){
+
+    private static float obtainingValuesforWorkingTime(MongoCollection userCollection, Product product) throws ParseException {
         int workingTime = product.getWorkingTime();
-        float currentSalary = Float.valueOf(String.valueOf(Profile.getValueFromUser(userCollection,1,"currentSalary"))); 
-        float workingHourCost = (float) ((currentSalary/30)/8);
-        float workingTimeCostPerProduct = workingHourCost * workingTime;
-        return workingTimeCostPerProduct;
-    } 
-    
-    public static float computeTotalProductProductionCost( MongoCollection materialsCollection, MongoCollection userCollection, Product product){
-        float totalProductProductionCost = computeMaterialsCostPerProduct(materialsCollection, product) + computeWorkingTimeCostPerProduct(userCollection,product);
+        float currentSalary = Float.valueOf(String.valueOf(Profile.getValueFromUser(userCollection, 1, "currentSalary")));
+        return computeWorkingTimeCostProduct(currentSalary, workingTime);
+
+    }
+
+    public static float computeTotalProductProductionCost(MongoCollection materialsCollection, MongoCollection userCollection, Product product) throws ParseException {
+        float totalProductProductionCost = computeMaterialsCostPerProduct(materialsCollection, product) + obtainingValuesforWorkingTime(userCollection, product);
         return totalProductProductionCost;
     }
-    private static float roundToTwoDecimalPlaces(float number) throws ParseException{
+
+    private static float roundToTwoDecimalPlaces(float number) throws ParseException {
         BigDecimal bd = new BigDecimal(number);
         bd = bd.setScale(2, RoundingMode.HALF_UP);
         double roundedNumber = bd.doubleValue();
         float convertedNumber = Float.parseFloat(String.valueOf(roundedNumber));
         return convertedNumber;
+    }
+
+    public static float computeWorkingTimeCostProduct(float currentSalar, int working) throws ParseException {
+        int workingTime = working;
+        float currentSalary = currentSalar;
+        float workingHourCost = (float) ((currentSalary / 30) / 8);
+        float workingTimeCostPerProduct = workingHourCost * workingTime;
+        return workingTimeCostPerProduct = roundToTwoDecimalPlaces(workingTimeCostPerProduct);
     }
 }
